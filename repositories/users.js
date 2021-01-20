53b1df2df84a2e89092c793a3dfa439e6dcc50f8
Jordan Stevens <jordangstvns@gmail.com>
2021-01-19 22:45:29 -0500

    first commit

---
+++ repositories/users.js
@@ -0,0 +1,38 @@
+const { json } = require("express");
+const fs = require("fs");
+const crypto = require("crypto");
+const { get } = require("http");
+const util = require("util");
+const Repository = require("./repository");
+
+scrypt = util.promisify(crypto.scrypt);
+
+class UsersRepository extends Repository {
+  async create(attrs) {
+    attrs.id = this.randomId();
+
+    const salt = crypto.randomBytes(8).toString("hex");
+    const buf = await scrypt(attrs.password, salt, 64);
+    const records = await this.getAll();
+    const record = {
+      ...attrs,
+      password: `${buf.toString("hex")}.${salt}`,
+    };
+    records.push(record);
+
+    await this.writeAll(records);
+
+    return record;
+  }
+
+  async comparePasswords(saved, supplied) {
+    //saved => password saved in database. 'hashed.salt'
+    //supplied => password given to us by user logging in
+    const [hashed, salt] = saved.split(".");
+    const suppliedBuffer = await scrypt(supplied, salt, 64);
+
+    return hashed === suppliedBuffer.toString("hex");
+  }
+}
+
+module.exports = new UsersRepository("users.json");
